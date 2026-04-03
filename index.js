33a9e02c037aebae2da4373c96fee3b7e2063c67
Sagar9347 <bendalamsagar8@gmial.com>
2026-04-03 22:43:10 +0530

    azure demo files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Azure!</h1><p>This is your demo app running on App Service.</p>');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
